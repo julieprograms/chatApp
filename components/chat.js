ec9ed1c179909809d8f5a40058cd5b927b092c0e
julieprograms <junge.julie@web.de>
2022-02-01 18:26:16 +0100

    added colorbg for chat

--- components/chat.js
+++ components/chat.js
@@ -4,14 +4,19 @@ import { View, Text, StyleSheet} from 'react-native';
 
 export class Chat extends React.Component {
   render() {
-    let name = this.props.route.params.name; // OR ...
+    let name = this.props.route.params.name;
+    let bgColor = this.props.route.params.bgColor;
+  // OR ...
     // let { name } = this.props.route.params;
 
     this.props.navigation.setOptions({ title: name });
 
+
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
+        <View style={{...styles.container, backgroundColor: bgColor}}>
+  
+        <Text style={styles.textLight}>Hello</Text></View>
       </View>
     )
   }
@@ -20,7 +25,13 @@ export class Chat extends React.Component {
 const styles = StyleSheet.create({
     container: {
        flex:1,
+       height: '100%',
+       width: '100%',
        justifyContent: 'center', 
-       alignItems: 'center' 
+       alignItems: 'center',
+    },
+    textLight: {
+      color: '#FFFFFF',
+      fontSize: 30,
     } 
  })
